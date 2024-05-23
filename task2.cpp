b892e542c73036178e7d756853509d397eaa4efb
webusinka <veronikabykova05@gmail.com>
2024-05-23 18:19:51 +0700

    Added decryption algorithm in task 2

--- task2.cpp
+++ task2.cpp
@@ -15,13 +15,22 @@ void generate_master_key(vector<unsigned char>& MASTER_KEY, mt19937_64& mt) {
     }
 }
 
-void shift_rows(std::vector<unsigned char>& line) {
+void shift_rows(vector<unsigned char>& line) {
     vector<unsigned char> v(line.size());
     for (int i = 1; i < line.size(); i++) {
         v[i - 1] = line[i];        //сдвиг элементов влево
     }
     v[line.size() - 1] = line[0]; //последний элемент становится первым
 
+    line = v;
+}
+
+void inv_shift_rows(vector<unsigned char>& line) {
+    vector<unsigned char> v(line.size());
+
+    for (int i = 0; i < line.size(); i++) {
+        v[i] = line[(i + (i % 4) * (i % 4)) % line.size()];
+    }
     line = v;
 }
 
@@ -53,6 +62,33 @@ void sub_bytes(vector<unsigned char>& line) { //замена на соответ
     line = v;
 }
 
+void inv_sub_bytes(vector<unsigned char>& line) { //замена на соответствующие байты из обратной S-BOX
+
+    vector<unsigned char> inv_sbox_data = {
+        0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+        0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+        0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+        0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+        0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+        0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+        0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+        0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+        0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+        0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+        0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+        0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+        0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+        0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+        0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+        0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+    };
+
+    vector<unsigned char> v;
+    for (auto i : line) {
+        v.push_back(inv_sbox_data[i]);
+    }
+    line = v;
+}
 
 vector<unsigned char> add_round_key(vector<unsigned char>& FIRST, vector<unsigned char>& SECOND) {
     vector<unsigned char> v;
@@ -61,7 +97,6 @@ vector<unsigned char> add_round_key(vector<unsigned char>& FIRST, vector<unsigne
     }
     return v;
 }
-
 
 void key_expansion(vector<unsigned char>& MASTER_KEY, vector<vector<unsigned char>>& ROUND_KEYS) {
 
@@ -153,6 +188,38 @@ vector<unsigned char> generate_mult_by_3() {
     return mult_by_3;
 }
 
+vector<unsigned char> generate_mult_by_14() {
+    vector<unsigned char> mult_by_14(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_14[i] = galois_multiply(i, 14);
+    }
+    return mult_by_14;
+}
+
+vector<unsigned char> generate_mult_by_9() {
+    vector<unsigned char> mult_by_9(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_9[i] = galois_multiply(i, 9);
+    }
+    return mult_by_9;
+}
+
+vector<unsigned char> generate_mult_by_13() {
+    vector<unsigned char> mult_by_13(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_13[i] = galois_multiply(i, 13);
+    }
+    return mult_by_13;
+}
+
+vector<unsigned char> generate_mult_by_11() {
+    vector<unsigned char> mult_by_11(256);
+    for (int i = 0; i < 256; ++i) {
+        mult_by_11[i] = galois_multiply(i, 11);
+    }
+    return mult_by_11;
+}
+
 void mix_column(vector<unsigned char>& line) {
     vector<unsigned char> mult_by_2 = generate_mult_by_2(); //операции умножения на 2 и 3 к столбцам матрицы данных
 
@@ -163,6 +230,20 @@ void mix_column(vector<unsigned char>& line) {
     v.push_back(mult_by_2[line[1]] ^ mult_by_3[line[2]] ^ line[0] ^ line[3]);
     v.push_back(mult_by_2[line[2]] ^ mult_by_3[line[3]] ^ line[0] ^ line[1]);
     v.push_back(mult_by_2[line[3]] ^ mult_by_3[line[0]] ^ line[1] ^ line[2]);
+    line = v;
+}
+
+void inv_mix_columns(vector<unsigned char>& line) {
+    vector<unsigned char> mult_by_14 = generate_mult_by_14();
+    vector<unsigned char> mult_by_9 = generate_mult_by_9();
+    vector<unsigned char> mult_by_13 = generate_mult_by_13();
+    vector<unsigned char> mult_by_11 = generate_mult_by_11();
+
+    vector<unsigned char> v;
+    v.push_back(mult_by_14[line[0]] ^ mult_by_9[line[1]] ^ mult_by_13[line[2]] ^ mult_by_11[line[3]]);
+    v.push_back(mult_by_14[line[1]] ^ mult_by_9[line[2]] ^ mult_by_13[line[3]] ^ mult_by_11[line[0]]);
+    v.push_back(mult_by_14[line[2]] ^ mult_by_9[line[3]] ^ mult_by_13[line[0]] ^ mult_by_11[line[1]]);
+    v.push_back(mult_by_14[line[3]] ^ mult_by_9[line[0]] ^ mult_by_13[line[1]] ^ mult_by_11[line[2]]);
     line = v;
 }
 
@@ -210,6 +291,36 @@ vector<vector<unsigned char>> CIPHER(vector<vector<unsigned char>>& BLOCK, vecto
         shift_rows(v[j]);
         v[j] = add_round_key(v[j], ROUND_KEYS[10]);
     }//ROUNDS_KEYS[10] - забрали
+
+    return v;
+}
+
+vector<vector<unsigned char>> DECIPHER(vector<vector<unsigned char>>& BLOCK, vector<vector<unsigned char>>& ROUND_KEYS, vector<unsigned char> MASTER_KEY) {
+    vector<vector<unsigned char>> v(4, vector<unsigned char>(4, 0));
+
+    // Первый этап - AddRoundKey с последним раундовым ключом
+    for (int j = 0; j <= 3; j++) {
+        v[j] = add_round_key(BLOCK[j], ROUND_KEYS[10]);
+    }
+
+    // Циклический этап - InvShiftRows, InvSubBytes, InvMixColumns, AddRoundKey
+    for (int i = 9; i >= 1; i--) {
+        for (int j = 0; j <= 3; j++) {
+            inv_shift_rows(v[j]);
+            inv_sub_bytes(v[j]);
+            inv_mix_columns(v[j]);
+            v[j] = add_round_key(v[j], ROUND_KEYS[i]);
+        }
+    }
+
+    // Последний этап - InvShiftRows, InvSubBytes, AddRoundKey
+    for (int j = 0; j <= 3; j++) {
+        inv_shift_rows(v[j]);
+        inv_sub_bytes(v[j]);
+    }
+    for (int j = 0; j <= 3; j++) {
+        v[j] = add_round_key(v[j], ROUND_KEYS[0]);
+    }
 
     return v;
 }
@@ -292,7 +403,7 @@ int main() {
     cout << "\n----------------------------------------------\n";
 
     PREV = DEFOLT;
-    vector<vector<vector<unsigned char>>> DECRYPT;
+    /*<vector<vector<unsigned char>>> DECRYPT;
 
     for (int i = 0; i < ECRYPT.size(); ++i) {
         vector<vector<unsigned char>> res(4, vector<unsigned char>(4, 0));
@@ -307,11 +418,26 @@ int main() {
         }
         DECRYPT.push_back(res);
         PREV = TEK;
+    }*/
+
+    vector<vector<vector<unsigned char>>> DECRYPT;
+
+    for (int i = 0; i < ECRYPT.size(); ++i) {
+        vector<vector<unsigned char>> res(4, vector<unsigned char>(4, 0));
+        vector<vector<unsigned char>> B = ECRYPT[i];
+
+        TEK = DECIPHER(B, ROUND_KEYS, MASTER_KEY);
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                res[i][j] = TEK[i][j];
+            }
+        }
+        DECRYPT.push_back(res);
     }
 
-    cout << "Message after encryption:\n" << endl;
+    cout << "Message after decryption:\n" << endl;
     for (int i = 0; i < DECRYPT.size(); i++) {
-
         vector<vector<unsigned char>>& decrypt = DECRYPT[i];
         for (int j = 0; j < decrypt.size(); j++) {
             for (int k = 0; k < decrypt[j].size(); k++) {
